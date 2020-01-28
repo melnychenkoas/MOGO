eeee061536c8274fbd861a2c56f1fef112837cea
melnychenkoas <nastya.melnychenko@gmail.com>
2020-01-28 22:11:59 +0200

    add width to testimonal slider

--- testimonial-slider.js
+++ testimonial-slider.js
@@ -1,23 +1,35 @@
 // export * from './testimonial-slider.js';
 let turn = 0;
-let sliderArrowLeft = [...document.getElementsByClassName('quote_arrow_left')];
-let sliderArrowRight = [...document.getElementsByClassName('quote_arrow_right')];
+const sliderArrowLeft = [...document.getElementsByClassName('quote_arrow_left')];
+const sliderArrowRight = [...document.getElementsByClassName('quote_arrow_right')];
+const slider = [...document.getElementsByClassName('slider')];
+
+console.log(slider);
+
+
 
 export const pushSlides = (side) => {
-    const slider = document.querySelector('.slider_container');
+    const sliderWidth= slider[1].offsetWidth;
+    console.log(sliderWidth);
+    
+    const sliderContainer = document.querySelector('.slider_container');
+    const sliderElements = document.querySelectorAll('.slider_element');
+    const slidesQuantity = sliderElements.length - 1;
     if(side === 'right') {
-        turn = turn - 1161;
+        turn = turn - sliderWidth;
     } else {
-        turn = turn + 1161;
+        turn = turn + sliderWidth;
     }
-    if(turn < -2322 ) {
+    if(turn < -slidesQuantity*sliderWidth ) {
         turn = 0
     } else if (turn > 0) {
-        turn = -2322
+        turn = -slidesQuantity*sliderWidth
     }
     console.log(turn)
-    slider.style = 'transform: translateX(' + turn + 'px)'; 
+    console.log(sliderContainer);
+    sliderContainer.style = 'transform: translateX(' + turn + 'px)'; 
 }
+
   sliderArrowLeft[0].addEventListener('click', () => {
     pushSlides('left');
 });
